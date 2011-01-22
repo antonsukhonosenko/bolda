09cd2edeb5c37ec3315f5f3a302fba2a0f25a7af
Anton Sukhonosenko <admin@MacBook-MB466.local>
2011-01-22 10:47:41 +0200

    fixed multibyte string encoding issues

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,13 @@
 # coding: utf-8
 
+require 'rubygems'
+require 'iconv'
+class String
+  def to_my_utf8
+    ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', self + ' ')[0..-2]
+  end
+end
+
 class GamesController < ApplicationController
 
   def new
@@ -8,14 +16,14 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(params[:game])
-    @game.row_letters = @game.letters.force_encoding('UTF-8').length
+    @game.row_letters = @game.letters.to_my_utf8.length
 
     if @game.save
 
       volume = @game.row_letters * (@game.row_letters - 1)
 
       @game.letters = ''.ljust(volume, '-') # create String of pre-defined amount of chars
-      @game.letters = @game.letters.insert(@game.row_letters * (@game.row_letters / 2), params[:game][:letters].force_encoding('UTF-8'))
+      @game.letters = @game.letters.insert(@game.row_letters * (@game.row_letters / 2), params[:game][:letters].to_my_utf8)
 
       @game.save!
 
@@ -51,10 +59,10 @@ class GamesController < ApplicationController
   def letter  # shit of a hell. mysql doesn't set up. sqlite doesn't SAVE db saying it does
     @game = Game.find(params[:game])
 
-    letters = @game.letters.force_encoding('UTF-8')
-    letters[params[:position].to_i-1] = params[:letter].force_encoding('UTF-8')
+    letters = @game.letters.to_my_utf8
+    letters[params[:position].to_i-1] = params[:letter].to_my_utf8
 
-    if @game.update_attributes!(:letters => letters.force_encoding('UTF-8')) # not saved for some reason? SQLite?
+    if @game.update_attributes!(:letters => letters.to_my_utf8) # not saved for some reason? SQLite?
       render :text => @game.letters
     end
   end
