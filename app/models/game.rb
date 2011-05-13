9c3fbd14a64500cc82693d0bb9e2a1e2fc0ff144
Anton Sukhonosenko <anton.sukhonosenko@gmail.com>
2011-05-13 15:55:38 +0300

    first for github

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,8 @@ class Game < ActiveRecord::Base
   # validates_format_of :letters, :with => /\A[a-zA-Zа-яА-ЯøØåÅ]+\z/u, :message => "only allowed"
 
   validates :row_letters, :numericality => true
+
+  # game_type: dictionary, postvote, prevote
+  # state: 0 = not_started, 1 = in_progress, 2 = finished
+
 end
